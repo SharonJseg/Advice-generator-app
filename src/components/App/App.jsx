1a06362d523e789a5fc5f77f8309efc5dc4f452b
SharonJseg <sharon.j.segal@gmail.com>
2022-04-05 23:18:52 +0300

    Update App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,21 +2,43 @@ import './App.css';
 import divider from '../../images/pattern-divider-desktop.svg';
 import mobileDivider from '../../images/pattern-divider-mobile.svg';
 import dice from '../../images/icon-dice.svg';
+import { useEffect, useState } from 'react';
 
 const App = () => {
+  const [advice, setAdvice] = useState({});
+
+  const fetchAdvice = async () => {
+    try {
+      const response = await fetch('https://api.adviceslip.com/advice');
+      const data = await response.json();
+      setAdvice(data.slip);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const onClickHandler = () => {
+    fetchAdvice();
+  };
+
+  useEffect(() => {
+    fetchAdvice();
+  }, []);
+
   return (
     <div className='page'>
       <article className='advice__container'>
-        <h2 className='advice__number'>advice #117</h2>
-        <p className='advice__text'>
-          “It is easy to sit up and take notice, what's difficult is getting up
-          and taking action.”
-        </p>
+        <h2 className='advice__number'>{`advice #${advice.id}`}</h2>
+        <p className='advice__text'>{`“${advice.advice}”`}</p>
         <div>
           <img className='advice__divider' src={divider} alt='' />
           <img className='advice__mobile-divider' src={mobileDivider} alt='' />
         </div>
-        <button className='advice__button'>
+        <button
+          type='button'
+          className='advice__button'
+          onClick={onClickHandler}
+        >
           <img className='dice' src={dice} alt='' />
         </button>
       </article>
